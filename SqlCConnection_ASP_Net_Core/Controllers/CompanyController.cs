c0dcdbc50e0b4cf92d4ecb496db3090f38bf0eeb
Leon Diekmann <Leon_Diekmann@web.de>
2018-09-13 17:45:41 +0200

    Added Get and Get by Id function

---
+++ SqlCConnection_ASP_Net_Core/Controllers/CompanyController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SqlCConnection_ASP_Net_Core.Models;
+using SqlCConnection_ASP_Net_Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SqlCConnection_ASP_Net_Core.Controllers
+{
+    [Route("companies")]
+    public class CompanyController : Controller
+    {
+        [HttpGet()]
+        public IActionResult Get()
+        {
+            var result = CompanyRepo.Read();
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var result = CompanyRepo.ReadByID(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] Company company)
+        {
+            var result = CompanyRepo.AddOrUpdate(company);
+            if (result == null)
+            {
+
+            }
+            return Ok(result);
+        }
+
+    }
+}
---
+++ SqlCConnection_ASP_Net_Core/Repository/CompanyRepo.cs
@@ -0,0 +1,68 @@
+﻿using SqlCConnection_ASP_Net_Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+
+namespace SqlCConnection_ASP_Net_Core.Repository
+{
+    public class CompanyRepo
+    {
+        public static List<Company> Read()
+        {
+            using (SqlConnection conn = new SqlConnection(Properties.Resources.connString))
+            {
+                string query = @"   SELECT Id, 
+                                    Name, 
+                                    PostCode, 
+                                    City, 
+                                    Street, 
+                                    HouseNumber, 
+                                    Country FROM [dbo].[viCompany]";
+
+                var companyList = conn.Query<Company>(query).ToList();
+                return companyList;
+            }
+
+        }
+
+        public static Company ReadByID(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(Properties.Resources.connString))
+            {
+                string query = @"   SELECT Id, 
+                                    Name, 
+                                    PostCode, 
+                                    City, 
+                                    Street, 
+                                    HouseNumber, 
+                                    Country FROM [dbo].[viCompany] WHERE Id = @Id";
+                var param = new DynamicParameters();
+                param.Add("@Id", id);
+
+                var company = conn.QueryFirstOrDefault<Company>(query, param);
+
+                return company;
+            }
+        }
+
+        public static Company AddOrUpdate(Company company)
+        {
+            using (SqlConnection conn = new SqlConnection(Properties.Resources.connString))
+            {
+                string companySelect = "spCompany";
+
+                var param = new DynamicParameters();
+                param.Add("@Id", company.Id);
+                param.Add("@Name", company.Name);
+
+                var companyResult = conn.QueryFirstOrDefault<Company>(companySelect, param, null, null, CommandType.StoredProcedure);
+
+                return companyResult;
+            }
+        }
+    }
+}
